b6a76744116ab86a8ef7bd52f4fcfa45989db4eb
Sakamoto, Kazunori <exKAZUu@users.noreply.github.com>
2019-11-15 15:53:22 +0900

    fix(gen-configs): updator for dependabot (#173)

--- packages/gen-configs/src/utils/mergeUtil.ts
+++ packages/gen-configs/src/utils/mergeUtil.ts
@@ -1,3 +1,20 @@
-export function overwriteMerge(destinationArray: any, sourceArray: any): any {
+import merge from 'deepmerge';
+
+export function overwriteMerge(destinationArray: any[], sourceArray: any[]): any[] {
   return sourceArray;
+}
+
+export function combineMerge(target: any[], source: any[], options: any): any[] {
+  const destination = target.slice();
+
+  source.forEach((item, index) => {
+    if (typeof destination[index] === 'undefined') {
+      destination[index] = options.cloneUnlessOtherwiseSpecified(item, options);
+    } else if (options.isMergeableObject(item)) {
+      destination[index] = merge(target[index], item, options);
+    } else if (target.indexOf(item) === -1) {
+      destination.push(item);
+    }
+  });
+  return destination;
 }

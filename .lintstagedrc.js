6604036b74aeca3c8d4a2a595ac827cbc23b3f9f
Sakamoto, Kazunori <exKAZUu@users.noreply.github.com>
2019-12-10 00:18:25 +0900

    chore: willboosterify this repo upgrading dependencies (#207)

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,7 @@
+const micromatch = require('micromatch');
 module.exports = {
   './**/*.{css,htm,html,js,json,jsx,md,scss,ts,tsx,vue,yaml,yml}': files => {
-    const filtered = files.filter(file => !file.includes('/test-fixtures/')).join(' ');
-    return filtered ? [`prettier --write ${filtered}`, `git add ${filtered}`] : [];
+    const fileList = files.filter(file => !file.includes('/test-fixtures/')).join(' ');
+    return fileList ? [`prettier --write ${fileList}`, `git add ${fileList}`] : [];
   },
 };

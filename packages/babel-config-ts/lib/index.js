409097f6cf48f9945e187e0f5c7ad9959aece765
Sakamoto, Kazunori <exkazuu@gmail.com>
2021-09-30 13:30:48 +0900

    .

--- packages/babel-config-ts/lib/index.js
+++ packages/babel-config-ts/lib/index.js
@@ -1,29 +1,38 @@
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        // Keep import statements for tree-shaking
-        modules: false,
-        useBuiltIns: 'usage',
-        corejs: '3.14',
-        bugfixes: true,
-      },
-    ],
-    '@babel/preset-react',
-    '@babel/typescript',
-  ],
-  plugins: ['@babel/proposal-class-properties', '@babel/proposal-numeric-separator'],
-  env: {
-    production: {
-      plugins: [
-        [
-          'transform-remove-console',
-          {
-            exclude: ['error', 'info', 'warn'],
-          },
-        ],
+const packageJson = require('../package.json');
+const coreJsVersion = packageJson.peerDependencies['core-js'] || '';
+const [major, minor, patch] = coreJsVersion.replace('^', '').split('.');
+const corejs = major && minor && patch ? `${major}.${minor}` : '3';
+
+module.exports = function (api) {
+  api.cache(true);
+
+  return {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          // Keep import statements for tree-shaking
+          modules: false,
+          useBuiltIns: 'usage',
+          corejs,
+          bugfixes: true,
+        },
       ],
+      '@babel/preset-react',
+      '@babel/typescript',
+    ],
+    plugins: ['@babel/proposal-class-properties', '@babel/proposal-numeric-separator'],
+    env: {
+      production: {
+        plugins: [
+          [
+            'transform-remove-console',
+            {
+              exclude: ['error', 'info', 'warn'],
+            },
+          ],
+        ],
+      },
     },
-  },
+  };
 };

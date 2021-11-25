376ca05ae18ac40bb55d2ad1bd851c4137ec4c13
Sakamoto, Kazunori <exkazuu@gmail.com>
2021-11-25 11:16:13 +0900

    chore: willboosterify this repo

--- packages/renovate-config/.lintstagedrc.cjs
+++ packages/renovate-config/.lintstagedrc.cjs
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  './**/*.{css,htm,html,js,json,json5,jsx,md,scss,ts,tsx,vue,yaml,yml}': (files) => {
+  './**/*.{cjs,css,htm,html,js,json,json5,jsx,md,mjs,scss,ts,tsx,vue,yaml,yml}': (files) => {
     const filteredFiles = files
       .filter((file) => !file.includes('/test-fixtures/'))
       .map((file) => path.relative('', file));

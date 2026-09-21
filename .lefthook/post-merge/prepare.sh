b9738ce071becad5610533923260584009e85a4d
Sakamoto, Kazunori <exkazuu@gmail.com>
2026-09-22 02:01:20 +0900

    chore: willboosterify this repo
    Applied @willbooster/wbfy v20.17.2.

--- .lefthook/post-merge/prepare.sh
+++ .lefthook/post-merge/prepare.sh
@@ -9,5 +9,6 @@ run_if_changed() {
 }
 
 run_if_changed "(mise\.toml|\.mise\.toml)" "mise install"
-if git diff --no-color -U0 ORIG_HEAD HEAD -- '*bunfig.toml' | grep --quiet -E '^[+-] *(globalStore|linker|publicHoistPattern)'; then rm -Rf -- 'node_modules' 'packages/babel-configs/node_modules' 'packages/biome-config/node_modules' 'packages/eslint-config-js-react/node_modules' 'packages/eslint-config-js/node_modules' 'packages/eslint-config-next/node_modules' 'packages/eslint-config-ts-react/node_modules' 'packages/eslint-config-ts/node_modules' 'packages/oxfmt-config/node_modules' 'packages/oxlint-config/node_modules' 'packages/prettier-config/node_modules' 'packages/shared/node_modules'; fi
+eval "$(mise env -s bash)"
+if git diff --no-color -U0 ORIG_HEAD HEAD -- '*bunfig.toml' | grep --quiet -E '^[+-] *(globalStore|linker|publicHoistPattern)'; then rm -Rf -- node_modules packages/babel-configs/node_modules packages/biome-config/node_modules packages/eslint-config-js-react/node_modules packages/eslint-config-js/node_modules packages/eslint-config-next/node_modules packages/eslint-config-ts-react/node_modules packages/eslint-config-ts/node_modules packages/oxfmt-config/node_modules packages/oxlint-config/node_modules packages/prettier-config/node_modules packages/shared/node_modules; fi
 run_if_changed "(package\.json|bun\.lock|bunfig\.toml|\.npmrc|patches/)" "bun install"
